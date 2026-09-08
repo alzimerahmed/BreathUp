a6242e8e17c33ad225b4df6b3cb1a675de1073b9
Alzimer Ahmed <alzimerahmed84@gmail.com>
2026-09-08 21:58:03 +0530

    fix(ci): ktlint multiline expression fix, suppress AGP library constraints warning

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -26,9 +26,10 @@ android {
     }
 
     val keystorePropsFile = file("C:/Users/shadd/keystores/BreathUp-keystore.properties")
-    val keystoreProps = Properties().apply {
-        if (keystorePropsFile.exists()) keystorePropsFile.inputStream().use { load(it) }
-    }
+    val keystoreProps = Properties()
+        .apply {
+            if (keystorePropsFile.exists()) keystorePropsFile.inputStream().use { load(it) }
+        }
 
     signingConfigs {
         if (keystorePropsFile.exists()) {
